c416b262dd2285f9485bc8c825bb878e16611445
Odrigor <rodcid.al@gmail.com>
2021-02-17 18:21:30 -0300

    SolucionadoImagenes&Stars

--- src/Item.js
+++ src/Item.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import './Item.css'
 
+class Item extends React.Component {
 
-function Item(props) {
-    return(
-        <div className="item">
-                <div className="image"><img src={'img/' + props.image} width="100%" /></div>
-                <div className="title">{props.title}</div>
-                <div className="rating">
-                    Calificación: 
-                    <select value={props.rating}>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                    </select>
+    constructor(props){
+        super(props);
+
+        this.state={
+            stars:[]
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            stars: Array(parseInt(this.props.rating)).fill(0)
+        });
+    }
+    
+    render() { 
+        return(
+            <div className="item">
+                    <div className="image"><img src={'img/' + this.props.image} width="100%" /></div>
+                    <div className="title">{this.props.title}</div>
+                    <div className="rating">
+                        <p>
+                        {
+                            this.state.stars.map( x => 
+                                <img src='img/star.png' width='32'></img>
+                            )
+                        }
+                        </p>
+                        Calificación: 
+                        <select value={this.props.rating}>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+    
                 </div>
-
-            </div>
-    );
+        );
+    }
 }
-
+ 
 export default Item;
