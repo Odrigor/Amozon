db7ebab971b319d789bd469806c854aa6ea84339
Odrigor <rodcid.al@gmail.com>
2021-02-18 22:51:53 -0300

    Finalizado

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-function Search(props) {
-    return(
-        <input type="text"></input>
-    );
+class Search extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.onChangeEvent = this.onChangeEvent.bind(this);
+    }
+
+    onChangeEvent(e){
+        const query = e.target.value.toString().toLowerCase();
+        this.props.onsearch(e.target.value);
+    }
+
+    render(){
+        return(
+            <input type="text" onChange={this.onChangeEvent} />
+        );
+    }
 }
 
 export default Search;
